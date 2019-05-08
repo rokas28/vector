2050d633e9384d06f48d26021cd1b2c47a64cbca
RokasT <rokas.tamulevicius28@gmail.com>
2019-05-08 15:55:09 +0300

    update

--- main.cpp
+++ main.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
 #include "vector.h"
 
+using std::cout;
+using std::endl;
+
 int main() {
 
-    //vec::vector<int> v1;
-    vec::vector<int> v{ 5, 10 };
-    v.push_back(2);
-    std::cout << v.size() << std::endl;
-    std::cout << v[0] << std::endl;
+    vec::vector<int> v;
+    vec::vector<int> v1(3, 10);
+    vec::vector<int> v2(5);
+    vec::vector<int> v3{ 5, 10, 20, 8, 8, 9 };
+
+    cout << v1.size() << endl;
+    cout << v1.capacity() << endl;
+
+    v1.push_back(2);
+    v1.push_back(5);
+    v1.push_back(3);
+    v1.push_back(6);
+    v1.push_back(7);
+    v1.push_back(8);
+    v1.push_back(9);
+    v1.push_back(4);
+
+    cout << v1.size() << endl;
+    cout << v1.capacity() << endl;
+    cout << v1[3] << endl;
 
     return 0;
 }
--- vector.h
+++ vector.h
@@ -14,12 +14,12 @@ namespace vec{
         typedef T *iterator;
         typedef T &reference;
 
-        vector() : n{ 0 }, elem{ new T[n] }, cap{ 0 } {};
+        vector() : n{ 0 },elem{ new T[1] }, cap{ 1 } {};
         vector(size_t n, T val) : n{ n }, elem{ new T[n] }, cap{ n } { std::fill_n(elem, n, val); }
         vector(size_t n) : n{ n }, elem{ new T[n] }, cap{ n } {};
         vector(std::initializer_list<T> il)
                 : n{ static_cast<std::size_t>(il.size()) },
-                  cap{ n },
+                  cap{ static_cast<std::size_t>(il.size()) },
                   elem{ new T[il.size()] } {
             std::copy(il.begin(), il.end(), elem);
         }
@@ -40,6 +40,8 @@ namespace vec{
             if (i < 0 || size() <= i) throw std::out_of_range {"Vector::operator[]"};
             return elem[i];
         }
+
+        int capacity() const { return cap; }
 
         void push_back(double x) {
             if (n >= cap) resize(2 * cap);
