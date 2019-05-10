b66690fff968f9f0778cba698d71c7f0a57bb3dc
RokasT <rokas.tamulevicius28@gmail.com>
2019-05-10 13:54:59 +0300

    operators< >

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <vector>
 
 int main() {
 
@@ -6,82 +7,22 @@ int main() {
     vec::vector<int> v1(3,10);
     vec::vector<int> v2(5);
     vec::vector<int> v3{ 5, 10, 20, 8, 8, 9, 5, 4, 10};
-    cout << "vector" << endl;
 
-    v1.swap(v3);
-    for(auto i : v1) cout << i << " ";
-    cout << endl;
-    for(auto i : v3) cout << i << " ";
-    cout << endl;
-    /*auto it = v3.begin();
-    v3.erase(it+1,it+4);
-    for(auto i : v3) cout << i << " ";
-    cout << endl;*/
-    /*auto it = v3.begin();
-    v3.insert(it,{5,2,3,6,8,9,8,7});
-    for(auto i : v3) cout << i << " ";
-    cout << endl;*/
-    /*it = v3.begin();
-    v3.insert(it+2,66);
-    for(auto i : v3) cout << i << " ";
-    cout << endl;
-    cout << v3.size() << " " << v3.capacity() << endl;
-    cout << "-" << endl;*/
-    /*v.assign({5,2,3,6,8,9,8,7});
-    for(auto i : v) cout << i << endl;*/
-    /*v3.reserve(20);
-    cout << v3.capacity() << endl;
-    v3.shrink_to_fit();
-    cout << v3.capacity() << endl;
-    for(int i =0; i < v3.size(); i++) cout << v3[i] << endl;*/
-    /*for(int i =0; i < v3.size(); i++) cout << v3[i] << endl;
-    v3.clear();
-    for(int i =0; i < v3.size(); i++) cout << v3[i] << endl;
-    cout << v3.capacity() << endl;*/
-    /*if (v1==v3) cout << "lygus" << endl;
-    if (v1!=v3) cout << "nelygus" << endl;*/
-    /*v3.erase(3);
-    for(int i =0; i < v3.size(); i++) cout << v3[i] << endl;*/
-    /*v1.push_front(5);
-    for(auto i : v1) cout << i << endl;*/
-    /*auto it = v3.begin();
-    v3.insert(it+3,3);
-    for(int i =0; i < v3.size(); i++) cout << v3[i] << endl;*/
-    //for(auto i : v3) cout << i << endl;
-    /*v3.pop_back();
-    v3.insert(2,4);
-    v3.insert(2,3,6);
-    for(auto i : v3) cout << i << endl;*/
-    /*v.assign(3,20);
-    cout << v[2] << endl;*/
-    /*v.assign(3,20);
-    for(auto i : v){
-        cout << i << endl;
-    }*/
-    /*cout << v3.begin() << endl;
-    cout << v3.end() << endl;*/
-    /*v = v3;
-    cout << v[2] << endl;
-    cout << v3[2] << endl;*/
-    /*cout << v1.size() << endl;
-    cout << v1.capacity() << endl;
-    cout << v.empty() << endl;
-    cout << v1.empty() << endl;*/
-    /*v3.at(2) = 50;
-    cout << v3[2] << endl;
-    cout << v3.back() << endl;
-    cout << v3.front() << endl;*/
-    /*v1.push_back(2);
-    v1.push_back(5);
-    v1.push_back(3);
-    v1.push_back(6);
-    v1.push_back(7);
-    v1.push_back(8);
-    v1.push_back(9);
-    v1.push_back(4);
-    cout << v1.size() << endl;
-    cout << v1.capacity() << endl;
-    cout << v1[3] << endl;*/
+    std::vector<int>a{5,50,5};
+    std::vector<int>b{5,50,5,10};
+
+    vec::vector<int>c{5,50,5,10,5};
+    vec::vector<int>d{1,50,5,10};
+
+    if (a<b) std::cout << "a<b\n";
+    if (a>b) std::cout << "a>b\n";
+    if (a<=b) std::cout << "a<=b\n";
+    if (a>=b) std::cout << "a>=b\n";
+
+    if ((c<d)) std::cout << "c<d\n";
+    if (c>d) std::cout << "c>d\n";
+    /*if (c<=d) std::cout << "c<=d\n";
+    if (c>=d) std::cout << "c>=d\n";*/
 
     return 0;
 }
--- vector.h
+++ vector.h
@@ -14,6 +14,12 @@ namespace vec{
     template < typename T >
     bool operator!= (const vector <T>&, const vector <T>&);
 
+    template < typename T >
+    bool operator< (const vector <T>&, const vector <T>&);
+
+    template < typename T >
+    bool operator> (const vector <T>&, const vector <T>&);
+
     template <typename T >
     class vector {
     private:
@@ -94,7 +100,7 @@ namespace vec{
         int capacity() const { return cap; }
 
         bool empty()const noexcept{
-            if (n!=0) return true;
+            if (n==0) return true;
             else return false;
         }
 
@@ -287,6 +293,8 @@ namespace vec{
 
         friend bool operator== <T> (const vector <T>& left, const vector <T>& right);
         friend bool operator!= <T> (const vector <T>& left, const vector <T>& right);
+        friend bool operator< <T> (const vector <T>& left, const vector <T>& right);
+        friend bool operator> <T> (const vector <T>& left, const vector <T>& right);
 
     };
 
@@ -302,6 +310,32 @@ namespace vec{
     template < typename T >
     bool operator!= (const vector <T>& left, const vector <T>& right) {
         return !(left == right);
+    }
+
+    template < typename T >
+    bool operator< (const vector <T>& left, const vector <T>& right) {
+
+        if(left.empty() && right.empty()) return false;
+        else if (left.empty()) return true;
+        else if (right.empty()) return false;
+        else {
+            int i = 0;
+            while(left[i]==right[i]){
+                if((i+1 > left.n)||(i+1 > right.n)) break;
+                i++;
+            }
+            if(left[i] < right[i]) return true;
+            else if(left[i] == right[i]){
+                if(left.n < right.n) return true;
+                else return false;
+            }
+            else return false;
+        }
+    }
+
+    template < typename T >
+    bool operator> (const vector <T>& left, const vector <T>& right) {
+        return !(left < right);
     }
 
 }
